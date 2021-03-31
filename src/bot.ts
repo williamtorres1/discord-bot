203f6c38c12e9c9580436bfbe4eeb7b9cb38f914
William Torres <contatowilliamtorres@gmail.com>
2021-03-30 23:05:33 -0300

    readd yarn.lock remove dependencies not used add log when connected

--- src/bot.ts
+++ src/bot.ts
@@ -1,19 +1,32 @@
 import Discord from 'discord.js';
-import config from './config/credentials.json';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const client = new Discord.Client();
 
 const prefix = '-';
 
-client.login(config.bot_token);
+client.once('ready', () => {
+  console.log(`uiu-bot running! Connected on ${client.user.tag}`);
+});
 
 client.on('message', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong.');
+  }
   if (message.author.bot) return;
   // futura feature
   // if (message.author.username === 'zSkrlipez') {
   // }
 
   // Toda vez que Bia falar com o bot, começar o comando com "Oi patroa, ..."
+  if (
+    message.content.startsWith(prefix) &&
+    message.author.id === process.env.id
+  ) {
+    const startMessage = 'Oi, pessoa muito importante.';
+    message.channel.send(`${startMessage} O que você deseja?`);
+  }
   if (!message.content.startsWith(prefix)) return;
 
   const commandBody = message.content.slice(prefix.length);
@@ -34,3 +47,5 @@ client.on('message', message => {
     message.reply(`TCHAU GENTE`);
   }
 });
+
+client.login(process.env.BOT_TOKEN);
